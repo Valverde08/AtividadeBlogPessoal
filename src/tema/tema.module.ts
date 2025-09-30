799ab7f32728e2369da756f47c229d79a53c2845
unknown <valerde88@outlook.com>
2025-09-30 00:24:37 -0300

    Correção de bugs

--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tema } from './entities/tema.entitiy';
+import { TemaService } from './service/tema.service';
 import { TemaController } from './controller/tema.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tema])],
-  providers: [],
-  controllers: [],
+  providers: [TemaService],
+  controllers: [TemaController],
   exports: [],
 })
 export class TemaModule {}
