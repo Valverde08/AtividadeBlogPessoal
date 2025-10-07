4d8469ee321d1b8a4c4ff9f9c6ef6d55b2efc024
unknown <valerde88@outlook.com>
2025-10-06 23:54:11 -0300

    Criação do module auth e adição no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { Tema } from './tema/entities/tema.entitiy';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { Tema } from './tema/entities/tema.entitiy';
     }),
     PostagemModule,
     TemaModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
